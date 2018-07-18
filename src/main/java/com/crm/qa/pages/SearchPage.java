bde6f7e3a730c75899eff8e8ece75a26d51a46d7
Stallone <stallonepereira24@gmail.com>
2018-07-19 00:16:05 +0530

    Initial Commit

---
+++ src/main/java/com/crm/qa/pages/SearchPage.java
@@ -0,0 +1,41 @@
+package com.crm.qa.pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.globals.Globals;
+
+public class SearchPage extends Globals{
+	public SearchPage(WebDriver driver){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Define the page Objects
+	
+		@FindBy(xpath="//*[contains(@title, 'Search')]")
+		public WebElement searchIcon;
+		
+		@FindBy(xpath="//*[contains(@id, 'searchInput')]")
+		public WebElement searchBox;
+		
+		@FindBy(xpath="//*[contains(@id, 'search_submit_btn')]")
+		public WebElement searchButton;
+		
+		@FindBy(xpath="//h1[@class='e_catalogtitle']")
+		public WebElement searchResult;
+		
+		@FindBy(xpath="//h1[@class='e_catalogtitle']")
+		public WebElement searchResultTwo;
+		
+		//Banner object
+		@FindBy(xpath="//*[contains(@class, 'owl-dot active')]")
+		public WebElement bannerSlider;
+		
+		@FindBy(xpath="//*[starts-with(@id, 'myCarousel')]")
+		public WebElement bannerOne;
+		
+		@FindBy(xpath="//*[contains(@class, 'new-in-banner')]")
+		public WebElement bannerTwo;
+		
+	}
